9b19f913be385b59586ce4cd58c5dc532ae9f7eb
bulgarigorr <bulgarigor@yandex.com>
2017-11-02 13:28:22 +0100

    Added: - user test case - mocha-typescript, superagent and expect.js

---
+++ src/test/user.test.ts
@@ -0,0 +1,7 @@
+import * as expect from 'expect.js';
+import * as superAgent from 'superagent';
+import {describe, it, timeout} from "mocha-typescript";
+
+describe ('/user API endpoint', () => {
+    it ('')
+});
