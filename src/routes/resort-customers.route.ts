bfa34a9df387a29dc3237de04fc96b7b563a7c79
bulgarigorr <bulgarigor@yandex.com>
2017-12-05 14:52:19 +0100

    MVE-70 Resort routes changes

--- src/routes/resort-customers.route.ts
+++ src/routes/resort-customers.route.ts
@@ -1,5 +1,6 @@
 import * as Express from 'express';
 import * as bodyParser from 'body-parser';
+import { IResortCustomer, IResortCustomerDetails } from '../database/resort-customers/resort-customers.model';
 
 export class ResortCustomersRoute {
     router: Express.Router;
@@ -9,19 +10,113 @@ export class ResortCustomersRoute {
         this.router = Express.Router();
 
         this.router.get('/all', (req, res) => {
-            res.status(200).json(this.mockResortCustomersList());
+            const result = this.mockResortCustomersList();
 
-            // this.dao.getLists()
-            //     .then((result) => {
-            //         res.status(200).json(result.lists);
-            //     })
-            //     .catch((error) => {
-            //         res.status(500).send(error);
-            //     });
+            res.status(200).json(result);
+        });
+        this.router.get('/detail', (req, res) => {
+            res.status(400).send('Resort id missing!');
+        });
+        this.router.get('/detail/:resortId', (req, res) => {
+            const resortId = req.params.resortId;
+            const result = this.mockResortCustomerDetails(resortId);
+            if (result) {
+                res.status(200).json(result);
+            } else {
+                res.status(500).json('Resort not found!');
+            }
         });
     }
 
-    private mockResortCustomersList() {
+    private mockResortCustomerDetails(id: string) {
+        const mock = this.mockResortCustomersList().find(r => r.id === id);
 
+        if (mock) {
+            mock['phone'] = '+354 588 5522';
+            mock['emailsSent'] = 3814;
+            mock['emailsOpened'] = 2123;
+            mock['bounceRate'] = 0.12;
+            mock['cancellations'] = 12;
+            mock['lastWeek'] = 223;
+        }
+
+        return mock;
+    }
+
+    private mockResortCustomersList(): IResortCustomer[] {
+        return [
+            {
+                id: '1',
+                name: 'Gistihús Gumma',
+                position: 'Guesthouse',
+                email: 'gummi69@simnet.is',
+                rooms: 4,
+                lastSent: '2017/01/12',
+                invoice: '$860'
+            },
+            {
+                id: '2',
+                name: 'Hótel Harpa',
+                position: 'Hotel',
+                email: 'sigrune@harpa.is',
+                rooms: 342,
+                lastSent: '2017/01/25',
+                invoice: '$112,000'
+            },
+            {
+                id: '3',
+                name: 'Hótel Hellnir',
+                position: 'Hotel',
+                email: 'hellnir@hellnir.is',
+                rooms: 61,
+                lastSent: '2017/04/25',
+                invoice: '$320'
+            },
+            {
+                id: '4',
+                name: 'Hótel Rangá',
+                position: 'Guesthouse',
+                email: 'gydasol@ranga.is',
+                rooms: 63,
+                lastSent: '2017/07/25',
+                invoice: '$1700'
+            },
+            {
+                id: '5',
+                name: 'Hótel Venus',
+                position: 'Guesthouse',
+                email: 'elli@hotmale.com',
+                rooms: 12,
+                lastSent: '2017/03/29',
+                invoice: '$4330'
+            },
+            {
+                id: '6',
+                name: 'Lónkot Bucolic Resort',
+                position: 'Guesthouse',
+                email: 'lonkot@lonkot.is',
+                rooms: 7,
+                lastSent: '2007/11/28',
+                invoice: '$162'
+            },
+            {
+                id: '7',
+                name: 'Snorralaug',
+                position: 'Hotel',
+                email: 'snorri@betel.com',
+                rooms: 12,
+                lastSent: '2017/12/22',
+                invoice: '$8690'
+            },
+            {
+                id: '8',
+                name: 'Snæfell Hotel',
+                position: 'Hotel',
+                email: 'snaefell@snaefell.is',
+                rooms: 51,
+                lastSent: '2017/11/13',
+                invoice: '$183,000'
+            }
+        ];
     }
 }
