724ae651bd65811cf146683a28b0c9b7cd99bf4d
bulgarigorr <bulgarigor@yandex.com>
2018-05-02 22:07:24 -0400

    mongo url

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class DbConfig {
     constructor() {
-        this.mongoUrl = 'mongodb://<birkheroku>:<birkheroku123>@ds111410.mlab.com:11410/heroku_skp20nm1';
+        this.mongoUrl = 'mongodb://birkheroku:birkheroku123@ds111410.mlab.com:11410/heroku_skp20nm1';
     }
 }
 exports.DbConfig = DbConfig;
