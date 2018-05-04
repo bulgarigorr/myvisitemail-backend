bd5ed4e822fef7e618452a7a653a829ced59916f
bulgarigorr <bulgarigor@yandex.com>
2018-05-04 00:42:09 -0400

    1

--- app.js
+++ app.js
@@ -31,6 +31,17 @@ const options = {
     methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
     credentials: true
 };
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.get('/', function(req, res, next) {
+  // Handle the get for this route
+});
+app.post('/', function(req, res, next) {
+ // Handle the post for this route
+});
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json())
 app.use(Express.static(process.env.PWD + '/'));
