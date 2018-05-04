809d47c008addc491b5c460eed6d5b4bfde7c9f1
bulgarigorr <bulgarigor@yandex.com>
2018-05-04 00:47:17 -0400

    1

--- app.js
+++ app.js
@@ -36,11 +36,9 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
 app.get('/', function(req, res, next) {
   // Handle the get for this route
 });
-
 app.post('/', function(req, res, next) {
  // Handle the post for this route
 });
